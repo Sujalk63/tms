48e30e94e25e50a80305c994de72f90c6023d8bb
Sujalk63 <sujalkarmakar6363@gmail.com>
2024-05-09 03:27:31 -0700

    structured the files

---
+++ backend/routes/login.js
@@ -0,0 +1,13 @@
+const express = require("express") 
+const router = express.Router();
+const employeeLoginHandler = require("./employee")
+const adminLoginHandler = require("./admin")
+
+router.use("/admin", adminLoginHandler)
+router.use("/employee", employeeLoginHandler)
+
+module.exports = router;
+
+
+
+
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const loginHandler = require("./routes/login");
+const { json } = require('body-parser');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", loginHandler);
+
+app.listen(4000);
+console.log("server runnning at port 4000");
+
+
+// app.get("/adminLogin", (req, res) => {
+//     res.send("this is admin login")
+//   })

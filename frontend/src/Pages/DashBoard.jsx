b730711132005b4ba5d5680dc176d8dd5b4e49fc
Sujalk63 <sujalkarmakar6363@gmail.com>
2024-05-22 12:31:16 -0700

    add taskRoom button

--- frontend/src/Pages/DashBoard.jsx
+++ frontend/src/Pages/DashBoard.jsx
@@ -1,24 +1,28 @@
 import { TopNavBar } from "../Components/TopNavBar";
-import React, { useState } from "react";
-import axios from "axios";
-import { TaskRoomForm} from "../Components/TaskRoomForm";
-
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { TaskRoomForm } from "../Components/TaskRoomForm";
+// import { plus } from "@heroicons/react/24/solid";
 
 export const Dashboard = () => {
   const [showForm, setShowForm] = useState(false);
-  const [taskRooms, setTaskRooms] = useState([]);
-
-  const handleAddTaskRoom = (newTaskRoom) => {
-    setTaskRooms([...taskRooms, newTaskRoom]);
-  };
+  const [buttonIcon, setButtonIcon] = useState(faPlus);
 
   return (
     <div className="w-full h-screen bg-customBg">
       <TopNavBar username={"SujalAdmin"}></TopNavBar>
-      <TaskRoomForm></TaskRoomForm>
+      {/* <TaskRoomForm></TaskRoomForm> */}
+      {showForm ? <TaskRoomForm></TaskRoomForm> : null}
       {/* <h1>is this working</h1> */}
-      <button onClick={() => setShowForm(true)} className="absolute bottom-4 right-4 bg-customColorLight text-white shadow-md p-4 rounded-md z-50 hover:bg-customColor">
-        Add Task Room +
+      <button
+        onClick={() => {
+          setShowForm(!showForm);
+        }}
+        className="transition duration-100 ease-in-out min-w-48 flex justify-center items-center gap-1 font absolute bottom-4 right-4 bg-customSideColor hover:bg-customSideColorDark text-black shadow-md p-4 rounded-md z-50 hover:bg-customColor"
+      >
+        <span className="font-bold " >{showForm ? "Cancel Task Room" : "Insert Task Room"}</span>
+        <FontAwesomeIcon className="font-bold " icon={showForm ? faTimes : faPlus} />
       </button>
     </div>
   );
